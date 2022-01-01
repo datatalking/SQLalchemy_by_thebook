a8438ed1a821fee30d313a2c73c0fac9e68e9654
Andrew <paintstone@gmail.com>
2021-12-31 21:31:07 -0800

    added DB_ENGINE dict within MyDatabase class

--- User_Database.py
+++ User_Database.py
@@ -1,11 +1,25 @@
 # SOURCE https://thinkdiff.net/how-to-use-python-sqlite3-using-sqlalchemy-158f9c54eb32
 
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Intger, String, Meta, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, Meta, ForeignKey
 
 # Global Setup
-SQLITE = 'sqlite'
+SQLITE                  = 'sqlite'
+# MYSQL                 = 'mysql'  # TODO add tests that this isnt used
+# POSTGRESQL            = 'postgresql'  # TODO add tests that this isnt used
+# MICROSOFT_SQL_SERVER  = 'mssqlserver'  # TODO add tests that this isnt used
 
 # Table Names
-USERS       = 'users'
-ADDRESSES   = 'addresses'
+USERS                   = 'users'
+ADDRESSES               = 'addresses'
+
+
+class MyDatabase:
+	# http://docs.sqlalchemy.org/en/latest/core/engines.html
+	DB_ENGINE = {
+		SQLITE: 'sqlite:///{DB}',
+		# MYSQL: 'mysql://bob:ross@localhost/{DB}',
+		# POSTGRESQL: 'postgresql://bob:ross@localhost/{DB}',
+		# MICROSOFT_SQL_SERVER: 'mssql+pymssql://bob:ross@localhost:port/{DB}'
+
+	}
