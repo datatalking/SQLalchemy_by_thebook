0c2052dd7b39a013cfd4c68570afa38ef92a387c
Andrew Schell <paintstone@gmail.com>
2022-01-08 23:17:53 -0800

    added print_add_data function

--- data/User_Database.py
+++ data/User_Database.py
@@ -80,6 +80,23 @@ class MyDatabase:
 				except Exception as e:
 					print(e)
 		
+		
+		def print_all_data(self, table = '', query = ''):
+			query = query if query != '' else "SELECT * FROM '{}';".format(table)
+			print(query)
+			
+			with self.db_engine.connect() as connection:
+				try:
+					result = connection.execute(query)
+				except Exception as e:
+					print(e)
+				else:
+					for row in result:
+						print(row) # TODO add print(row[0], row[1], row[2])
+					result.close()
+			
+			print("\n")
+		
 		# Examples
 		
 		def sample_query(self):
