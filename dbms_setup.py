19654904b04838c3bb076199a586188712850fb6
Andrew Schell <paintstone@gmail.com>
2022-01-08 22:45:33 -0800

    added main and whole import file for database

---
+++ dbms_setup.py
@@ -0,0 +1,23 @@
+# SOURCE Muhmad Ahsan
+# SQLalchemy
+
+from database import mydatabase
+
+def main()
+	dbms = mydatabase.MyDatabase(mydatabase.SQLITE, dbname = 'mydb.sqlite') # TODO .format for SQLITE f string
+	
+	# create Tables
+	dbms.create_db_tables()
+	# dbms.insert_single_data()
+	dbms.print_all_data(mydatabase.USERS)
+	dbms.print_all_data(mydatabase.ADDRESSES)
+	
+	dbms.sample_query() # simple query
+	dbms.sample_delete() # delete data
+	dbms.sample_insert() # insert data
+	dbms.sample_update() # update data
+
+
+# run the program
+if __name__ == '__main__':
+	main()
